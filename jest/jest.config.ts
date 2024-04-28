328a7749d37feeae9259f6e91eca5e59092d5848
Riya Amemiya <riyaamemiya@gmail.com>
2024-04-29 01:11:09 +0900

    feat(jest設定): Jestの設定ファイルを追加

---
+++ jest/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+
+const config: Config = {
+	transform: {
+		"^.+\\.(t|j)sx?$": "@swc/jest",
+	},
+	testEnvironment: "node",
+	roots: ["<rootDir>/src/tests"],
+	cacheDirectory: "<rootDir>/node_modules/.cache/jest",
+	collectCoverage: true,
+	collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+	coverageDirectory: "./coverage",
+	moduleDirectories: ["node_modules", "src"],
+	moduleNameMapper: {
+		"@/(.*)": "<rootDir>/src/$1",
+	},
+};
+export default config;
